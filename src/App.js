3ce6c67f000968b4649d1c21174a3c51d3fc65d8
erayhacioglu <hacoglueray@gmail.com>
2022-12-10 22:25:44 +0300

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import toast from 'react-hot-toast';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [colors, setColors] = useState([]);
+
+	useEffect(() => {
+		generatePalette();
+	}, []);
+
+	const generateColor = () => {
+		let str = 'abcdef0123456789';
+		let color = ' #';
+		for (let i = 0; i <= 5; i++) {
+			color += str[Math.floor(Math.random() * str.length)];
+		}
+		return color;
+	};
+
+	const generatePalette = () => {
+		let color = '';
+		for (let i = 1; i <= 5; i++) {
+			color += generateColor();
+		}
+		setColors(color.split(' '));
+	};
+
+	return (
+		<div className='wrapper'>
+			<h1 className='title'>Color Palette Generator</h1>
+			<div className='cards'>
+				{colors?.map(
+					(color, key) =>
+						color && (
+							<div
+								className='card'
+								key={key}
+								onClick={() => {
+									navigator.clipboard.writeText(color);
+									toast.success(`Color ${color} copied to your clipboard`);
+								}}
+							>
+								<div className='color' style={{ '--card-color': color }}></div>
+								<h2 className='code'>{color}</h2>
+							</div>
+						)
+				)}
+			</div>
+			<button className='generate-btn' onClick={generatePalette}>
+				Generate Palette
+			</button>
+		</div>
+	);
+};
 
 export default App;
